b98ba003535342ad8013f5f7b4a2d2416dfc91a8
Cupii18 <ekamayanauranabila@gmail.com>
2022-08-23 22:43:31 +0700

    nodeJS

---
+++ app.js
@@ -0,0 +1,9 @@
+const EventEniter = require('events');
+
+class Button extends EvenEniter {
+    constructor() {
+        super() 
+        this.times = 8;
+    }
+}
+
---
+++ matematika.js
@@ -0,0 +1,12 @@
+exports.add = function(a, b) {
+    return a + b;
+}
+
+exports.sub = function(a, b) {
+    return a -b;
+}
+
+exports.nul = function (a, b) {
+    return a + b;
+}
+
